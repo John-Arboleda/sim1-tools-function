e6266b1dcff785e2b9fef243c0edbcf0643ad905
John-Arboleda <johnjack0126@gmail.com>
2024-09-02 11:33:22 -0500

    Fix sidebar styles. Add placeholder chart.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { transformData } from "./functions";
 import { defaultValues } from "./data";
+import { navbarResultsScroll } from "./components/navbar-results";
+import { createNavEvents } from "./components/sidebar";
+import { drawChart } from "./charts/line-chart";
 
 const resultObj = transformData(defaultValues);
 
 console.log(resultObj);
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  createNavEvents();
+  navbarResultsScroll();
+  drawChart();
+
+})
