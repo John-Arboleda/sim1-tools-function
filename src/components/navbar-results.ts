bfb068d06d6f6519577dbeb68237177f82c6ef94
John-Arboleda <johnjack0126@gmail.com>
2024-09-04 11:44:08 -0500

    Switch pages navbar links. Scroll results navbar.

---
+++ src/components/navbar-results.ts
@@ -0,0 +1,30 @@
+
+function navbarResultsScroll(): void {
+  const mainPanel = document.getElementById('main-panel') as HTMLElement;
+  // Get the navbar
+  const emissionsResultNavbar = document.getElementById('emissions-result-navbar') as HTMLElement;
+  const fleetResultNavbar = document.getElementById('fleet-result-navbar') as HTMLElement;
+  const costsResultNavbar = document.getElementById('costs-result-navbar') as HTMLElement;
+
+  // Add the sticky class to the navbar when you reach its scroll position.
+  // Remove "sticky" when you leave the scroll position
+  function makeStickyNavbar(navbar: HTMLElement): void {
+    // Get the offset position of the navbar
+    const sticky: number = navbar.offsetTop;
+
+    if (mainPanel.scrollTop > sticky) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  }
+
+  // When the user scrolls the page, execute makeStickyNavbar
+  mainPanel.onscroll = function (): void { 
+    makeStickyNavbar(emissionsResultNavbar); 
+    makeStickyNavbar(fleetResultNavbar); 
+    makeStickyNavbar(costsResultNavbar); 
+  };
+}
+
+export { navbarResultsScroll };
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { transformData } from "./functions";
 import { defaultValues } from "./data";
 import { navbarResultsScroll } from "./components/navbar-results";
-import { createNavEvents } from "./components/sidebar";
+import { createNavEvents} from "./components/main-navbar";
 import { drawChart } from "./charts/line-chart";
 
 const resultObj = transformData(defaultValues);
 
 console.log(resultObj);
+createNavEvents();
+navbarResultsScroll();
 
 document.addEventListener("DOMContentLoaded", function() {
 
-  createNavEvents();
+  
+  //createNavEvents();
   navbarResultsScroll();
   drawChart();
 
